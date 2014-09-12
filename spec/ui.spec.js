88ffa582beff55d5685e5ceafa6809f6b98c207e
Ranj Abass <ranjdar.abass@broadbandchoices.co.uk>
2014-09-12 10:45:30 +0100

    angular test clean up

--- spec/ui.spec.js
+++ spec/ui.spec.js
@@ -14,48 +14,46 @@ chrome = {
     }
   }
 };
+describe("Switcheroo", function(){
+	var sut, scope;
+	beforeEach(function(){
+		module('switcheroo');
+	})
 
-describe("switcheroo ui tests", function() {
-	var sut = new SwitcherooUI();
+	describe("RulesController", function() {
+		beforeEach(inject(function ($rootScope, $controller) {
+			scope = $rootScope.$new();
 
-	beforeEach(function(done){
-		jasmine.getFixtures().createContainer_();
-		$('#jasmine-fixtures').load('switcheroo.html #container', function(){
-      		//sut.init();
-      		
-      		done();
-     	});
-    });
+			sut = $controller('RulesController', {
+	                '$scope': scope
+	            });
+	    }));
 
-	it('should have empty new rules input', function(){
-		expect($('#fromInput')).not.toHaveValue();
+		it('should add a new rule when add button is clicked', function(){
+			givenRuleAdded('abc', 'def');
+			expect(scope.rules).toBe([{from: 'abc', to:'def', isActive:true }]);
+		});
+
+		it('should remove all rules when we click remove all button', function(){
+			givenRuleAdded('abc', 'def');
+			givenRuleAdded('uvw', 'xyz');
+			expect(scope.rules).toBe(2);
+			sut.clear();
+			expect(scope.rules).toBe(0);
+		});
+
+		it('should be able to edit rules', function(){
+			givenRuleAdded('abc', 'def');
+			givenRuleAdded('uvw', 'xyz');
+			$('.editRuleButton:eq(0)').click();
+			expect($('.edit-rule .fromInput')).toHaveValue('abc');
+			expect($('.edit-rule .toInput')).toHaveValue('def');
+		});
+
+		function givenRuleAdded(from, to){
+			scope.from = from;
+			scope.to = to;
+			scope.add();
+		}
 	});
-
-	it('should add a new rule when add button is clicked', function(){
-		angular.bootstrap(document, ['switcheroo']);
-		givenRuleAdded('abc', 'def');
-		expect($('#rules li').length).toBe(1);
-	});
-
-	it('should remove all rules when we click remove all button', function(){
-		givenRuleAdded('abc', 'def');
-		givenRuleAdded('uvw', 'xyz');
-		expect($('#rules li').length).toBe(2);
-		$('#removeAllRulesButton').click();
-		expect($('#rules li').length).toBe(0);
-	});
-
-	it('should be able to edit rules', function(){
-		givenRuleAdded('abc', 'def');
-		givenRuleAdded('uvw', 'xyz');
-		$('.editRuleButton:eq(0)').click();
-		expect($('.edit-rule .fromInput')).toHaveValue('abc');
-		expect($('.edit-rule .toInput')).toHaveValue('def');
-	});
-
-	function givenRuleAdded(from, to){
-		$('#fromInput').val('abc');
-		$('#toInput').val('def');
-		$('#addRuleButton').click();
-	}
 });
--- switcheroo-ui.js
+++ switcheroo-ui.js
@@ -1,98 +1,27 @@
 angular.module('switcheroo', [])
-  .controller('RulesController', ['$scope', function($scope) {
-    $scope.rules = chrome.runtime.getBackgroundPage().rules;
- 
-    $scope.add = function() {
-      $scope.rules.push({
-		from: this.from,
-		to: this.to,
-		isActive: true
-	  });
-    };
-
-    $scope.edit = function(index, rule) {
-		$scope.rules[index] = rule;
-	};
-
-	$scope.remove = function(index) {
-		$scope.rules.splice(index,1);
-	};
-
-	$scope.clear = function() {
-		$scope.rules = [];
-	};
- 
-  }]);
-
-var SwitcherooUI = function(){
-	var ruleManager;
-
-	this.init = function() {
-		ruleManager = new SwitcherooCore(chrome.runtime.getBackgroundPage().rules);
-		rulesUl = $('#rules');
-		newRuleDiv = $('#new-rule');
-
-		$('#rules').delegate('.active', 'click', function() {
-			toggleRule(parseInt($(this).parent().attr('data-rule-index')));
-		});
-		
-
-		$('#rules').delegate('.editRuleButton', 'click', function() {
-
-			var ruleParent = $(this).parent();
-			var editIndex = parseInt(ruleParent.attr('data-rule-index'));
-		});	
-
-		$('#fromInput').focus();
-	};
-
-	var refreshRules = function() {
-		rulesUl.empty();
-		var rules = ruleManager.rules;
-		if(rules.length){
-			$('#no-rules').hide();
-			$('#headings').show();
-			for (var i = 0; i < rules.length; i++) {
-				var rule = rules[i];
-				var li = $('<li class="' + rule.isActive + '" data-rule-index="' + i + '"/>');
-				var fromSpan = '<span class="from" title="' + rule.from + '">' + textMinifier(rule.from) + '</span>';
-				var seperator = '<span class="seperator">&gt;</span>'
-				var toSpan = '<span class="to" title="' + rule.to + '">' + textMinifier(rule.to) + '</span>';
-				var checked = rule.isActive ? 'checked="checked"' : '';
-				var active = '<input type="checkbox" class="active" name="active" ' + checked + '/>'
-				var editLink = '<a href="#" class="editRuleButton">Edit</a>'
-				var removeLink = '<a href="#" class="removeRuleButton">Remove</a>';		
-				li.append(fromSpan + seperator + toSpan + active + editLink + removeLink);
-				rulesUl.append(li);
-			}
-		}
-		else
-		{
-			$('#no-rules').show();
-			$('#headings').hide();
-		}
-	};
-
-	var getNewRule = function(){
-		var fromInput, toInput, typeDropDown;
-
-		fromInput = newRuleDiv.children('#fromInput');
-		toInput = newRuleDiv.children('#toInput');
-
-		var newRule = {
-			from : fromInput.val(),
-			to : toInput.val(),
+  	.controller('RulesController', ['$scope', function($scope) {
+	    $scope.rules = chrome.runtime.getBackgroundPage().rules;
+	 
+	    $scope.add = function() {
+	      $scope.rules.push({
+			from: $scope.from,
+			to: $scope.to,
 			isActive: true
+		  });
+	    };
+
+	    $scope.edit = function(index, rule) {
+			$scope.rules[index] = rule;
 		};
 
-		refreshRules();
+		$scope.remove = function(index) {
+			$scope.rules.splice(index,1);
+		};
 
-		fromInput.val('');
-		toInput.val('');
-
-		return newRule;
-	};
-}
+		$scope.clear = function() {
+			$scope.rules = [];
+		};
+ }]);
 
 function textMinifier(text){
 		var maxLength = 25;
