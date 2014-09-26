79d036382b6b86f0fea7cb00ff79944e88fbd20e
ranjez <ranjez@gmail.com>
2014-09-26 08:33:23 +0100

    Added synced rules service

--- src/RulesService.js
+++ src/RulesService.js
@@ -1,11 +1,31 @@
 var LocalRulesService = function (){
 		this.get = function(){
-			return JSON.parse(localStorage['rules']);
+			var stroredRules = localStorage['rules'];
+
+			if(!stroredRules){
+				return [];
+			}
+
+			return JSON.parse(stroredRules);
 		};
-		this.set = function(data){
-			localStorage['rules'] = JSON.stringify(data);
+		this.set = function(rules){
+			localStorage['rules'] = JSON.stringify(rules);
 		};
 };
+
+var SyncedRulesService = function(){
+	this.get = function(){
+		var rules = [];
+		chrome.storage.sync.get('rules', function(item){
+			rules = item.rules;
+		});
+
+		return rules;
+	};
+	this.set = function(rules){
+		chrome.storage.sync.set({'rules': rules});
+	};
+}
 
 var RulesServiceFactory = {
 	getRulesService: function(){
