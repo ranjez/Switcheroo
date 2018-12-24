baa6128132b8f867a95b98c795079bebbcfa3c14
sherbang <brian@sherbang.com>
2018-12-24 15:28:15 +0000

    Don't apply redirects to username/password/pathname/search parts of url

--- src/RuleMatcher.js
+++ src/RuleMatcher.js
@@ -4,10 +4,16 @@ var RuleMatcher = function(rules){
     this.rules = rules;
 
     this.redirectOnMatch = function(request){
-        var rule = _.find(rules, function(rule){ 
-            return rule.isActive 
-            && request.url.indexOf(rule.from) > -1 
-            && request.requestId !== lastRequestId; 
+        var rule = _.find(rules, function(rule){
+            // Don't match user/pat/query parts of URL
+            var url = new URL(request.url);
+            url.username='';
+            url.password='';
+            url.pathname='';
+            url.search='';
+            return rule.isActive
+            && url.toString().indexOf(rule.from) > -1
+            && request.requestId !== lastRequestId;
         });
 
         if(rule){
